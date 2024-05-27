319027458198d5fec18a769fbf162e90baf7548b
Vishesh Gatha <visheshgatha@Visheshs-MacBook-Air.local>
2024-05-28 01:29:08 +0530

    initial

---
+++ components/Features/featuresData.tsx
@@ -0,0 +1,48 @@
+import { Feature } from "@/types/feature";
+
+const featuresData: Feature[] = [
+  {
+    id: 1,
+    icon: "/images/icon/icon-01.svg",
+    title: "Website Development",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  },
+  {
+    id: 2,
+    icon: "/images/icon/icon-02.svg",
+    title: "App development",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  },
+  {
+    id: 3,
+    icon: "/images/icon/icon-03.svg",
+    title: "Chatbot Development",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  },
+  {
+    id: 4,
+    icon: "/images/icon/icon-04.svg",
+    title: "E-commerce Development",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  },
+  {
+    id: 5,
+    icon: "/images/icon/icon-05.svg",
+    title: "Software Development",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  },
+  {
+    id: 6,
+    icon: "/images/icon/icon-06.svg",
+    title: "Digital Marketing",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  },
+];
+
+export default featuresData;
---
+++ components/FeaturesTab/featuresTabData.tsx
@@ -0,0 +1,30 @@
+import { FeatureTab } from "@/types/featureTab";
+
+const featuresTabData: FeatureTab[] = [
+  {
+    id: "tabOne",
+    title: "Expert Web Development Services",
+    desc1: `Unlock the potential of your online presence with our expert web development services. Our team specializes in crafting cutting-edge websites tailored to your unique needs and objectives. From sleek designs to robust functionality, we ensure your website stands out in the digital landscape.`,
+    desc2: ``,
+    image: "/images/features/features-light-01.png",
+    imageDark: "/images/features/features-dark-01.svg",
+  },
+  {
+    id: "tabTwo",
+    title: "Innovative App Development Solutions",
+    desc1: `Transform your ideas into reality with our innovative app development solutions. Whether you're targeting iOS, Android, or cross-platform development, our team has the expertise to bring your vision to life.`,
+    desc2: ``,
+    image: "/images/features/features-light-01.png",
+    imageDark: "/images/features/features-dark-01.svg",
+  },
+  {
+    id: "tabThree",
+    title: "Cutting-edge Chatbot Development",
+    desc1: `Enhance customer engagement and streamline processes with our cutting-edge chatbot development services. Whether you need a chatbot for customer support, lead generation, or task automation, our team can deliver tailored solutions to meet your specific requirements.`,
+    desc2: ``,
+    image: "/images/features/features-light-01.png",
+    imageDark: "/images/features/features-dark-01.svg",
+  },
+];
+
+export default featuresTabData;
